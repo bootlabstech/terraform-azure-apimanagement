5314300db04117ba36fafffb5c502f4d45e352f2
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-24 01:29:40 +0530

    add provider tf file

--- main.tf
+++ main.tf
@@ -2,23 +2,31 @@ resource "azurerm_api_management" "apim" {
   name                = var.apim_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  publisher_name      = "Bootlabs"
-  publisher_email     = "bootlabs@terraform.io"
-
-  sku_name = "Developer_1"
+  publisher_name      = var.publisher_name
+  publisher_email     = var.publisher_email
+  sku_name            = var.sku_name
+   lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
 
 resource "azurerm_api_management_api" "api" {
   name                = var.api_name
   resource_group_name = var.resource_group_name
   api_management_name = azurerm_api_management.apim.name
-  revision            = "1"
-  display_name        = "API"
-  path                = "api"
-  protocols           = ["https"]
+  revision            = var.revision
+  display_name        = var.display_name
+  path                = var.path
+  protocols           = var.protocols
 
   import {
-    content_format = "swagger-link-json"
-    content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
+    content_format = var.content_format
+    content_value  = var.content_value
   }
 }
+
+
+
+
